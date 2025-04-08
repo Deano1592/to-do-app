e549456786cbdd66a1eab6aee7f38a99ce2d249c
Dean Ridgwell <deano1592@hotmail.com>
2025-04-08 13:10:05 +0100

    Adding HTML and CSS code

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-const todoForm = document.querySelector("form");
-const todoInput = document.getElementById("todo-input");
-const todoListUL = document.getElementById("todo-list");
 
-let allTodos = [];
-
-todoForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  addTodo();
-});
-
-const addTodo = () => {
-  const todoText = todoInput.value.trim();
-  if (todoText.length > 0) {
-    allTodos.push(todoText);
-    todoInput.value = "";
-  }
-};
