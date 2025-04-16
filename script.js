3aaefc610236a96d34d9e0daff8bd51cf40ef229
Dean Ridgwell <deano1592@hotmail.com>
2025-04-16 15:11:50 +0100

    Adding HTML and CSS code

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const inputBox = document.getElementById("input-box");
+const button = document.querySelector("button");
+const listContainer = document.getElementById("list-container");
 
+button.addEventListener("click", addTask);
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+}
